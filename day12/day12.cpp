5fb6c8806e1e982d4f1244a9e6cae9c67629b4c1
Al Chambers <unicycle.bloke@gmail.com>
2023-12-12 06:23:44 +0000

    Preparing the input for Part 2 - now fix the algo

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -13,31 +13,19 @@ int matches(string corrupt, size_t index, const string& pattern)
 {
     int result = 0;
 
-    //cout << index << ' ' << corrupt << " .\n";
-
     if (index >= corrupt.size())
     {
-        //cout << corrupt << " => " << pattern << '\n';
-
         regex  re(pattern);
         smatch m;
-        bool is_match = regex_match(corrupt, m, re);
-        if (is_match)
-        {
-            //cout << corrupt << " => " << pattern << '\n';
-        }
-        
-        return is_match ? 1 : 0;
+        return regex_match(corrupt, m, re) ? 1 : 0;
     }
 
     while ((index < corrupt.size()) && (corrupt[index] != '?')) ++index;
     
-    corrupt[index] = 'x';
-    //cout << corrupt << " .\n";
+    corrupt[index] = '.';
     result += matches(corrupt, index, pattern);
 
     corrupt[index] = '#';
-    //cout << corrupt << " #\n";
     result += matches(corrupt, index, pattern);
 
     return result;
@@ -70,25 +58,6 @@ auto part2(T& input)
 }
 
 
-// vector<tuple<Args...>>
-//auto lines = aoc::read_lines<int,int,int,int>(filename, R"((\d+)-(\d+),(\d+)-(\d+))");
-
-// vector<string>    
-//auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
-
-// Replace all substrings matching "search" with "replace".
-//std::string replace(std::string source, const std::string& search, const std::string& replace);
-
-// Split a delimited string of tokens into a vector of string tokens. Trims substrings by default and drops trimmed 
-// tokens which are empty by default. Not convinced how useful the option are, but you never know.
-//std::vector<std::string> split(std::string source, std::string delim, Blanks allow_blanks = Blanks::Suppress, Trim trim_subs = Trim::Yes);
-
-// vector (size_type n, const value_type& val = value_type(),
-//vector<int> row(COLS, 0);
-//vector<vector<int>> grid(ROWS, row);
-//grid[row][col];
-
-
 void run(const char* filename)
 {
     auto lines = aoc::read_lines(filename);
@@ -100,32 +69,43 @@ void run(const char* filename)
         auto t = aoc::split(s[1], ",");
         ostringstream os;
 
-        // os << "\\.*";
-        // for (auto c: t)
-        // {
-        //     for (auto d: aoc::range(stoi(c))) os << '#';
-        //     os << "\\.+";
-        // }
-
-        os << "x*";
+        os << "\\.*";
         for (auto c: t)
         {
             for (auto d: aoc::range(stoi(c))) os << '#';
-            os << "x+";
+            os << "\\.+";
         }
 
-        auto x = aoc::replace(s[0], ".", "x");
-        reports.push_back({line, x + "x", os.str()});
-        //reports.push_back({line + ".", s[0], os.str()});
-
-        cout << s[0] << ' ' << s[1] << ' ' << os.str() << '\n';
+        reports.push_back({line, s[0] + ".", os.str()});
     }
 
     auto p1 = part1(reports);
     cout << "Part1: " << p1 << '\n';
     aoc::check_result(p1, 0);
 
-    auto p2 = part2(lines);
+    vector<Report> reports2;
+    for (auto line: lines)
+    {
+        auto s  = aoc::split(line, " ");
+        auto s0 = s[0] + "?" + s[0] + "?" + s[0] + "?" + s[0] + "?" + s[0];
+        auto s1 = s[1] + "," + s[1] + "," + s[1] + "," + s[1] + "," + s[1];    
+
+        cout << s0 << ' ' << s1 << '\n';    
+
+        auto t = aoc::split(s1, ",");
+        ostringstream os;
+
+        os << "\\.*";
+        for (auto c: t)
+        {
+            for (auto d: aoc::range(stoi(c))) os << '#';
+            os << "\\.+";
+        }
+
+        reports2.push_back({line, s0 + ".", os.str()});
+    }
+
+    auto p2 = part1(reports2);
     cout << "Part2: " << p2 << '\n';
     aoc::check_result(p2, 0);
 }
