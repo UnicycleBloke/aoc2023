aeba0fc087d533350af1e7f9f086d0eff222f09d
Al Chambers <unicycle.bloke@gmail.com>
2023-12-12 06:05:41 +0000

    Completed Day 12 Part 1 - very slow and won't do for Part 2

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -1,12 +1,63 @@
 #include "utils.h"
 
 
+struct Report
+{
+    string input;
+    string corrupt;
+    string pattern;
+};
+
+
+int matches(string corrupt, size_t index, const string& pattern)
+{
+    int result = 0;
+
+    //cout << index << ' ' << corrupt << " .\n";
+
+    if (index >= corrupt.size())
+    {
+        //cout << corrupt << " => " << pattern << '\n';
+
+        regex  re(pattern);
+        smatch m;
+        bool is_match = regex_match(corrupt, m, re);
+        if (is_match)
+        {
+            //cout << corrupt << " => " << pattern << '\n';
+        }
+        
+        return is_match ? 1 : 0;
+    }
+
+    while ((index < corrupt.size()) && (corrupt[index] != '?')) ++index;
+    
+    corrupt[index] = 'x';
+    //cout << corrupt << " .\n";
+    result += matches(corrupt, index, pattern);
+
+    corrupt[index] = '#';
+    //cout << corrupt << " #\n";
+    result += matches(corrupt, index, pattern);
+
+    return result;
+}
+
+
 template <typename T>
-auto part1(const T& input)
+auto part1(const T& reports)
 {
     aoc::timer timer;
 
-    return 0;
+    int total_ways = 0;
+    for (const auto& r: reports)
+    {
+        auto ways = matches(r.corrupt, 0, r.pattern);
+        cout << "ways = " << ways << '\n';
+        total_ways += ways;
+    }    
+
+    return total_ways / 2;
 }
 
 
@@ -35,11 +86,42 @@ auto part2(T& input)
 // vector (size_type n, const value_type& val = value_type(),
 //vector<int> row(COLS, 0);
 //vector<vector<int>> grid(ROWS, row);
+//grid[row][col];
+
+
 void run(const char* filename)
 {
     auto lines = aoc::read_lines(filename);
 
-    auto p1 = part1(lines);
+    vector<Report> reports;
+    for (auto line: lines)
+    {
+        auto s = aoc::split(line, " ");
+        auto t = aoc::split(s[1], ",");
+        ostringstream os;
+
+        // os << "\\.*";
+        // for (auto c: t)
+        // {
+        //     for (auto d: aoc::range(stoi(c))) os << '#';
+        //     os << "\\.+";
+        // }
+
+        os << "x*";
+        for (auto c: t)
+        {
+            for (auto d: aoc::range(stoi(c))) os << '#';
+            os << "x+";
+        }
+
+        auto x = aoc::replace(s[0], ".", "x");
+        reports.push_back({line, x + "x", os.str()});
+        //reports.push_back({line + ".", s[0], os.str()});
+
+        cout << s[0] << ' ' << s[1] << ' ' << os.str() << '\n';
+    }
+
+    auto p1 = part1(reports);
     cout << "Part1: " << p1 << '\n';
     aoc::check_result(p1, 0);
 
